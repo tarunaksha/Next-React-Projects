9858936bcd39c38b4c02f9a84094652a76631d8f
tarunaksha <tarun.garhbeta@gmail.com>
2023-09-26 22:23:28 +0530

    .

--- todo-list/app/page.js
+++ todo-list/app/page.js
@@ -4,13 +4,16 @@ import React, { useState } from "react";
 const page = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
-  const submitHandler = (e) =>{
-    e.preventDefault()
+  const [mainTask, setMainTask] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
     console.log(title);
     console.log(desc);
-    setTitle('');
-    setDesc('');
-  }
+    setTitle("");
+    setDesc("");
+    
+  };
+  let renderTask = <h2>No Tasks Available</h2>
   return (
     <>
       <div className="bg-black">
@@ -18,14 +21,15 @@ const page = () => {
           My Todo List
         </h1>
       </div>
+      <div className="flex justify-center">
       <form onSubmit={submitHandler}>
         <input
           type="text"
           className="border-4 border-black m-5 text-4xl p-4 rounded-3xl"
           placeholder="Title"
           value={title}
-          onChange={(e)=>{
-            setTitle(e.target.value)
+          onChange={(e) => {
+            setTitle(e.target.value);
           }}
         />
         <input
@@ -33,14 +37,20 @@ const page = () => {
           className="border-4 border-black m-5 text-4xl p-4 rounded-3xl"
           placeholder="Description"
           value={desc}
-          onChange={(e)=>{
-            setDesc(e.target.value)
+          onChange={(e) => {
+            setDesc(e.target.value);
           }}
         />
         <button className="bg-black text-white text-3xl p-5 rounded-3xl m-5">
           Add Task
         </button>
       </form>
+      </div>
+      
+  
+      <div className="p-8 bg-slate-200">
+        {renderTask}
+      </div>
     </>
   );
 };
