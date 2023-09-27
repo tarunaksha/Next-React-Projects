b432ab9d36639409122329a6348c9f234b594845
tarunaksha <b.tarunaksha@gmail.com>
2023-09-27 14:43:40 +0530

    final product

--- todo-list/app/page.js
+++ todo-list/app/page.js
@@ -7,13 +7,36 @@ const page = () => {
   const [mainTask, setMainTask] = useState([]);
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(title);
-    console.log(desc);
+    setMainTask([...mainTask, { title, desc }]);
     setTitle("");
     setDesc("");
-    
   };
-  let renderTask = <h2>No Tasks Available</h2>
+  const deleteHandler = (i) => {
+    let copyTask = [...mainTask];
+    copyTask.splice(i, 1);
+    setMainTask(copyTask);
+  };
+  let renderTask = <h2>No Tasks Available</h2>;
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((task, idx) => {
+      return (
+        <li key={idx} className="flex justify-between items-center mb-8">
+          <div className="flex justify-between w-2/3">
+            <h4 className="text-2xl font-semibold">{task.title}</h4>
+            <h5 className="text-lg font-medium">{task.desc}</h5>
+          </div>
+          <button
+            onClick={() => {
+              deleteHandler(idx);
+            }}
+            className="bg-red-500 text-white font-bold rounded px-5 py-2"
+          >
+            Delete
+          </button>
+        </li>
+      );
+    });
+  }
   return (
     <>
       <div className="bg-black">
@@ -22,35 +45,32 @@ const page = () => {
         </h1>
       </div>
       <div className="flex justify-center">
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          className="border-4 border-black m-5 text-4xl p-4 rounded-3xl"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          className="border-4 border-black m-5 text-4xl p-4 rounded-3xl"
-          placeholder="Description"
-          value={desc}
-          onChange={(e) => {
-            setDesc(e.target.value);
-          }}
-        />
-        <button className="bg-black text-white text-3xl p-5 rounded-3xl m-5">
-          Add Task
-        </button>
-      </form>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            className="border-4 border-black m-5 text-4xl p-4 rounded-3xl"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            className="border-4 border-black m-5 text-4xl p-4 rounded-3xl"
+            placeholder="Description"
+            value={desc}
+            onChange={(e) => {
+              setDesc(e.target.value);
+            }}
+          />
+          <button className="bg-black text-white text-3xl p-5 rounded-3xl m-5">
+            Add Task
+          </button>
+        </form>
       </div>
-      
-  
-      <div className="p-8 bg-slate-200">
-        {renderTask}
-      </div>
+
+      <div className="p-8 bg-slate-200">{renderTask}</div>
     </>
   );
 };
