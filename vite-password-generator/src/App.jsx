361c5576b8c4358e945e92783cc89a970b918646
tarunaksha <b.tarunaksha@gmail.com>
2023-09-30 19:47:13 +0530

    initials

---
+++ vite-password-generator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div className="h-screen w-full bg-slate-900">
+      <h1 className="text-8xl text-white text-center font-bold pt-4">Password Generator</h1>
+    </div>
+  )
+}
+
+export default App
